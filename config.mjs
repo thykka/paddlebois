bb6ea962e7b0b4caf960e3b59bccb5a6f2b2f7c4
Moses Holmström <thykka@gmail.com>
2020-05-16 16:02:08 +0300

    Boilerplate https/ws server with mustache.js

---
+++ config.mjs
@@ -0,0 +1,9 @@
+const Config = {
+  serverPort: 8000,
+  keyFile: './key.pem',
+  certFile: './cert.pem',
+  certPass: 'PaddleBois1', // TODO: use dotenv
+  wsServerPort: 8080,
+};
+
+export default Config;
---
+++ server.mjs
@@ -0,0 +1,43 @@
+import Config from './config.mjs';
+
+import path from 'path';
+import fs from 'fs';
+import https from 'https';
+import express from 'express';
+import mustacheExpress from 'mustache-express';
+import WebSocket from 'ws';
+
+const serverPath = path.resolve();
+const app = express();
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', `${ serverPath }/src/views`);
+
+const server = https.createServer({
+  key: fs.readFileSync(Config.keyFile),
+  cert: fs.readFileSync(Config.certFile),
+  passphrase: Config.certPass
+}, app);
+
+server.listen(Config.serverPort, () => {
+  console.log(`Listening at https://127.0.0.1:${ Config.serverPort }`);
+});
+
+app.get('/', (request, response) => {
+  response.render('index', { foo: 'bar' });
+});
+
+const wss = new WebSocket.Server({
+  port: Config.wsServerPort
+});
+
+wss.on('connection', handleConnection);
+
+
+function handleConnection(ws) {
+  ws.on('message', handleMessage);
+}
+
+function handleMessage(message) {
+  console.log(`message: ${ message }`);
+}
